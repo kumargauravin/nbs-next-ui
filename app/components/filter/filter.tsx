1014404002402e1654bf1e2286d0d735785bbf4f
Goyal Services <goyalservices@Macs-Mac-mini.local>
2023-12-18 03:30:54 -0500

    Changes to add basic structure of Filter

---
+++ app/components/filter/filter.tsx
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Grid } from '@mui/material';
+
+export default function BasicFilter() {
+    const [reportType, setReportType] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setReportType(event.target.value as string);
+  };
+
+    return (
+        <div>
+            <Accordion elevation={0}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Filter Options</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={4}> 
+                        <Box sx={{ minWidth: 120 }}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Report Type</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={reportType}
+                                    label="Report Type"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={"daily"}>Daily</MenuItem>
+                                    <MenuItem value={"weekly"}>Weekly</MenuItem>
+                                    <MenuItem value={"monthly"}>Monthly</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={4}> 
+                        <Box sx={{ minWidth: 120 }}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Channels</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={reportType}
+                                    label="Report Type"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={"daily"}>Daily</MenuItem>
+                                    <MenuItem value={"weekly"}>Weekly</MenuItem>
+                                    <MenuItem value={"monthly"}>Monthly</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
+                        </Grid> 
+                        <Grid item xs={12} sm={4}> 
+                        <Box sx={{ minWidth: 120 }}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Report Period</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={reportType}
+                                    label="Report Type"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value={"daily"}>Daily</MenuItem>
+                                    <MenuItem value={"weekly"}>Weekly</MenuItem>
+                                    <MenuItem value={"monthly"}>Monthly</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
+                        </Grid>                                               
+                    </Grid>
+                    <Grid container>
+                        <Grid item xs={12} sm={6}>
+                            {/* https://mui.com/x/react-date-pickers/ */}
+                        </Grid>
+                    </Grid>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
+}
--- app/vbg/page.tsx
+++ app/vbg/page.tsx
@@ -16,6 +16,7 @@ import { useParams } from 'next/navigation'
 // import { headers } from 'next/headers'
 import CustomTabPanel, { a11yProps } from '../components/tabs/tab-panels';
 import ReactTable from '../components/react-table/react-table';
+import BasicFilter from '../components/filter/filter';
 
 export default function VbgHome() {
   const [value, setValue] = React.useState(0);
@@ -39,17 +40,20 @@ export default function VbgHome() {
   return (
     <>
       <Box component="main">
-          <Box>
+          {/* <Box>
             QueryParams: {JSON.stringify(x)}
-          </Box>
+          </Box> */}
           {/* <Box>
             Headers: {JSON.stringify(y)}
           </Box> */}
-          <Box>
+          {/* <Box>
             PathParams: {JSON.stringify(pathParams)}
-          </Box>
+          </Box> */}
           <Box>
             <Grid container>
+              <Grid item xs={12}>
+                <BasicFilter/>
+              </Grid>
               <Grid item xs={12}>
                 <Box sx={{ width: '100%' }}>
                   <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
